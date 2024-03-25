0cf9c65c0170334c5bd82dc30e0248c94d4ac563
CrisDaniel <Cris@CRIS>
2024-03-25 14:40:57 -0700

    first commit

---
+++ Pagina11/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         int Num, Aux, Dec, Uni;
+        string Linea;
+        System.Console.WriteLine("Ingrese nuemero de dos cifras");
+       Num = int.Parse(Console.ReadLine());
+       Dec = Num/10;
+       Uni = Num %10;
+       Aux = (Uni*10)+Dec;
+       Console.WriteLine("Numero invertido eso " + Aux);
+       Console.ReadKey();
+    }
+}
