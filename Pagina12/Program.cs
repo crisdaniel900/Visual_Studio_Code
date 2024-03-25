8050e2ef4eb208117ebac66fe97236e9be271cf9
CrisDaniel <Cris@CRIS>
2024-03-25 14:43:37 -0700

    pagina12

---
+++ Pagina12/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+           int Num, Aux, Dec, Uni, Cen;
+        Console.WriteLine("Ingrese numero de 3 cirfras: ");
+        Num = int.Parse(Console.ReadLine());
+        Cen = Num/100;
+        Num = Num%100;
+        Dec = Num/10;
+        Uni = Num %10;
+        Aux = (Uni *100 ) + (Dec *10 )+  Cen;
+        Console.WriteLine ("Numero invertido es " + Aux);
+        Console.ReadKey();
+    }
+}
